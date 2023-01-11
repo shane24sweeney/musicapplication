aa8352239b426a478fcc2ac9c2de696e77634390
Shane Sweeney <shane24sweeney@gmail.com>
2023-01-11 13:58:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
+class App extends Component {
+    
+
+state = { artistQuery: ''};
+
+
+updateArtistQuery = event => {
+    this.setState({artistQuery: event.target.value});
+    console.log('event.target.value',event.target.value);
+
 }
+
+searchArtist = () => {
+    console.log('this.state',this.state);
+
+    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+    .then(response => response.json())
+    .then(json => {
+        console.log('json', json);
+        
+        if (json.artists.total > 0)
+        {
+            const artist = json.artists.items[0];
+            this.setState({artist});
+        }
+    });
+}
+
+handleKeyPress = event => {
+    if (event.onKeyPress == 'Enter')
+    {
+        this.searchArtist();
+    }
+    
+}
+   
+    render(){
+        return(
+        <div>
+            
+           <h2>Music Master</h2>
+           <input 
+           onChange = {this.updateArtistQuery}
+           onKeyPress = {this.handleKeyPress}
+           placeholder = 'Search for an artist' 
+           />
+           <button onClick={this.searchArtist}>Search</button>
+        
+        </div>
+        );
+       
+    
+        }      
+    
+}
+
+
+
 
 export default App;
